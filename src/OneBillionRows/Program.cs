40184ec8b17172d83805acaf3b4b72f22dfc1e5f
PurvPatel <purv810@gmail.com>
2024-02-26 22:37:38 -0500

    OneBillionRows Setup

---
+++ src/OneBillionRows/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Text;
+
+// read file
+const Int32 BufferSize = 128;
+using (var fileStream = File.OpenRead("../../data.txt"))
+{
+  using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+  {
+    String line;
+    while ((line = streamReader.ReadLine()) != null)
+    {
+      // Process line
+      Console.WriteLine(line);
+    }
+  }
+}
